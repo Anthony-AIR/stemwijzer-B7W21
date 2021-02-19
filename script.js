cc0838693e49e65296c47fa2f4f207ccacc2b895
anthony <99057187@mydavinci.nl>
2021-02-19 11:23:59 +0100

    alle antwoorden worden goed opgeslagen

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ startbutton.onclick = start;
 previous.onclick = previousStatement;
 eens.onclick = pro;
 geen.onclick = none;
-skip.onclick = none;
+skip.onclick = showSecularParties;
 oneens.onclick = contra;
 
 
@@ -46,32 +46,35 @@ function showStatements(){
         console.log(statementsanswer);
     }
     else{
-        statement++;
         document.getElementById("title").innerHTML = subjects[statement].title;
-        document.getElementById("statement").innerHTML = subjects[statement].statement; 
-        normaleButtonColor();
+        document.getElementById("statement").innerHTML = subjects[statement].statement;
     }
     
 }
 
 function pro(){
+    prevAnwsers();
     statementsanswer[statement] = "pro";
+    statement++;
     showStatements(); 
 }
 
 function contra(){
+    prevAnwsers();
     statementsanswer[statement] = "contra";
+    statement++;
     showStatements(); 
 }
 
 function none(){
+    prevAnwsers();
     statementsanswer[statement] = "none";
+    statement++;
     showStatements(); 
 }
 
 function previousStatement(){
     if (statement > 0 ){
-        statement--;
         statement--;
         prevAnwsers();
         showStatements();  
@@ -84,29 +87,47 @@ function prevAnwsers(){
         oneens.style.backgroundColor = "red";
         geen.style.backgroundColor = "#9E9E9E";
     }
-
     else if(statementsanswer[statement] == "contra"){
         oneens.style.backgroundColor = "blue";
         eens.style.backgroundColor = "green";
         geen.style.backgroundColor = "#9E9E9E";
-
     }
-
     else if(statementsanswer[statement] == "none"){
         geen.style.backgroundColor = "blue";
+        eens.style.backgroundColor = "green";
+        oneens.style.backgroundColor = "red";
+    }
+    else if(statementsanswer[statement] == undefined){
         oneens.style.backgroundColor = "red";
         eens.style.backgroundColor = "green";
+        geen.style.backgroundColor = "#9E9E9E";
     }
 }
 
-function normaleButtonColor(){
-    eens.style.backgroundColor = "green";
-    oneens.style.backgroundColor = "red";
-    geen.style.backgroundColor = "#9E9E9E";
+function showSecularParties(){
+    var i = 0;
+    parties.forEach(party => {
+        if(party.secular == true){
+            var elem = document.createElement("p");
+            elem.innerHTML = party.name;
+            elem.id = "party" + i;
+            container.appendChild(elem);
+            console.log("showSecularParties is een succes");
+        }
+    })
 }
-
-
 console.log(subjects);
-
-
-
+/*
+var i = 0;
+parties.forEach(party => {
+    if(party.secular == true){
+        var elem = document.createElement("input");
+        elem.innerhtml = party.name;
+        elem.id = "party" + i;
+        elem.setAttribute("type", "checkbox");
+        container.appendChild(elem);
+        console.log("showSecularParties is een succes");
+    }
+})
+}
+*/
